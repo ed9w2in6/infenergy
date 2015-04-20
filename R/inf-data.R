d896be8003b944cca8aa9a4808fa636b0103146d
David C Sterratt <david.c.sterratt@ed.ac.uk>
2015-04-20 15:02:33 +0100

    sys.Time() -> sys.time()

--- R/inf-data.R
+++ R/inf-data.R
@@ -47,7 +47,7 @@ get.inf.data <- function(date, ups="forumA", cache="prefer") {
   ## Otherwise, get data from system
   base.url <- "http://netmon.inf.ed.ac.uk/raw-UPS"
   year <- strftime(date, "%Y")
-  if (year != strftime(Sys.Time(), "%Y")) {
+  if (year != strftime(Sys.time(), "%Y")) {
     base.url <- file.path(base.url, year)
   }
   file <- file.path(base.url,
