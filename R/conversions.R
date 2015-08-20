2aa6d31a4cdac7eacb0ec235405bece2353fac0b
David C Sterratt <david.c.sterratt@ed.ac.uk>
2015-08-20 13:25:15 +0100

    Replaced get.uni.data.hourly wiht hourly.halfhourly

--- R/conversions.R
+++ R/conversions.R
@@ -19,11 +19,30 @@ hourly.cumulative <- function(x) {
   ## Bin into hourly chunks
   times <- seq.POSIXt(from, to, by="1 hour")
   dat <- approx(x$time, x$cumkwh, times)
-  out <- data.frame(Time=times[-1]-1800, kWh=diff(dat$y))
+  out <- data.frame(Time=times[-length(times)], kWh=diff(dat$y))
   class(out) <- c("hourly", class(out))
   attr(out, "from") <- attr(x, "from")
   attr(out, "to") <- attr(x, "to")
   return(out)
+}
+
+##' @export
+hourly.halfhourly <- function(x) {
+  ## Find the first and last time we can interpolate from
+  from <- as.POSIXlt(attr(x, "from"))
+  to <- as.POSIXlt(attr(x, "to"))
+
+  ## Bin into daily chunks, with the date boundary always being
+  ## midnight in any timezone, e.g. BST or GMT
+  times <- seq.POSIXt(from, to, by="1 hour")
+  bins <- cut(x$Time, times) # , labels=times[-1]-1800)
+
+  ad <- aggregate(kWh ~ bins, data=x, FUN=sum)
+  d <- with(ad, data.frame(Time=as.POSIXct(bins), kWh=kWh))
+  attr(d, "from") <- from
+  attr(d, "to") <- to
+  class(d) <-  c("hourly", class(d))
+  return(d)
 }
 
 ##' @title Daily representation of data
@@ -48,7 +67,7 @@ daily.hourly <- function(x) {
   ## Bin into daily chunks, with the date boundary always being
   ## midnight in any timezone, e.g. BST or GMT
   dates <- round(seq(from, to, by="1 day"), units="day")
-  bins <- cut(x$Time, dates, labels=seq.Date(as.Date(from), as.Date(to - 12*3600), by=1))
+  bins <- cut(x$Time, dates, labels=seq.Date(as.Date(from), as.Date(dates[length(dates) - 1]), by=1))
 
   ad <- aggregate(kWh ~ bins, data=x, FUN=sum)
   d <- with(ad, data.frame(Time=as.POSIXct(bins), kWh=kWh))
--- R/uni-data.R
+++ R/uni-data.R
@@ -7,6 +7,9 @@
 ##' @author David Sterratt
 ##' @export
 get.uni.half.hourly.data <- function(from, to) {
+  from <- as.POSIXlt(from)
+  to <- as.POSIXlt(to)
+
   files <- list.files("/home/sterratt/admin/inf-energy/data/uni-hh/", "\\.csv$",
                       full.names=TRUE)
   dat <- NULL
@@ -32,31 +35,10 @@ get.uni.half.hourly.data <- function(from, to) {
   dat$Time <- Time-15*60
   dat <- dat[order(dat$Time),]
   dat <- unique(dat)
-  dat <- subset(dat, (Time>=as.POSIXct(from, tz="GMT")) &
-                (Time <= as.POSIXct(to, tz="GMT") + 24*3600))
-  
+  dat <- subset(dat, (Time >= from) & (Time <= to))
+
+  attr(dat, "from") <- from
+  attr(dat, "to") <- to
+  class(dat) <- c("halfhourly", "data.frame")
   return(dat)
 }
-
-##' @title Get hourly data from the University Energy Office's data files
-##' @param from Date from which to collect data
-##' @param to Date to which to collect data
-##' @return Data frame with columns \code{Time} (the centre of the
-##' time period) and \code{kWh} (energy use in kWh in the hour
-##' centred on \code{Time})
-##' @author David Sterratt
-##' @export
-get.uni.data.hourly <- function(from, to) {
-  from <- as.POSIXlt(from)
-  to <- as.POSIXlt(to)
-  
-  dat <- get.uni.half.hourly.data(from, to)
-  fdat <- dat[seq(2,nrow(dat),2),]
-  fdat[,2] <- fdat[,2] + dat[seq(1,nrow(dat),2),2]
-  fdat[,1] <- dat[seq(1,nrow(dat),2),1] + 15*60
-  attr(fdat, "from") <- from
-  attr(fdat, "to") <- to
-  class(fdat) <- c("hourly", "data.frame")
-  return(fdat)
-}
-
